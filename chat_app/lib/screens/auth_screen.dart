bc32b045ea874a849aa0101c501e7a2b2221d441
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-04-07 21:29:18 +0800

    connect auth form

---
+++ chat_app/lib/screens/auth_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:chat_app/widgets/auth_form.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const AuthForm();
+  }
+}
