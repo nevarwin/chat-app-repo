8ef9fa83afc630f3feb84f031c4fcda50e6cbff4
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-04-11 21:42:03 +0800

    create a function to collect data from authForm

--- chat_app/lib/screens/auth_screen.dart
+++ chat_app/lib/screens/auth_screen.dart
@@ -10,8 +10,19 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  void _submitAuthForm(
+    String email,
+    String username,
+    String password,
+    bool isLogin,
+  ) {}
   @override
   Widget build(BuildContext context) {
-    return const AuthForm();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: AuthForm(
+        _submitAuthForm,
+      ),
+    );
   }
 }
