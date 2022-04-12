393a350c33398a2e2d490443fdff7e8f7d8e682c
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-04-12 22:34:59 +0800

    added submit email and password to firebase

--- chat_app/lib/screens/auth_screen.dart
+++ chat_app/lib/screens/auth_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:chat_app/widgets/auth_form.dart';
+import 'package:flutter/services.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
@@ -10,12 +12,51 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  void _submitAuthForm(
+  final auth = FirebaseAuth.instance;
+  Future<void> _submitAuthForm(
     String email,
     String username,
     String password,
     bool isLogin,
-  ) {}
+    // ctx for the scaffold snackbar
+    BuildContext ctx,
+  ) async {
+    UserCredential userCredential;
+    try {
+      if (isLogin) {
+        userCredential = await auth.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      } else {
+        userCredential = await auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+      }
+    }
+    // Platform specific error
+    on PlatformException catch (error) {
+      var message = 'An error occured. Please check your credentials.';
+
+      if (error.message != null) {
+        message = error.message!;
+      }
+
+      // Showing the error message in bottom snack bar
+      Scaffold.of(ctx).showSnackBar(
+        SnackBar(
+          content: Text(message),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+    }
+    // on typical error
+    catch (error) {
+      print(error);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
