ef5987da92855aa97e81ae87ed8fc67a17b52e92
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-04-13 21:55:14 +0800

    create messages.dart

---
+++ chat_app/lib/widgets/chat/messages.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Messages extends StatelessWidget {
+  const Messages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+      builder: (context, chatSnapshot) {
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final chatDocs = chatSnapshot.data!.documents;
+        return ListView.builder(
+          itemCount: chatDocs.length,
+          itemBuilder: (context, index) => Text(
+            chatDocs[index]['text'],
+          ),
+        );
+      },
+    );
+  }
+}
