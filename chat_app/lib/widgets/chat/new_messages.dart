1e990c23dff1abda150b1522ae24bf1d5479680d
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-04-14 22:07:15 +0800

    create newMessage dart

---
+++ chat_app/lib/widgets/chat/new_messages.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class NewMessages extends StatefulWidget {
+  const NewMessages({Key? key}) : super(key: key);
+
+  @override
+  State<NewMessages> createState() => _NewMessagesState();
+}
+
+class _NewMessagesState extends State<NewMessages> {
+  final _controller = TextEditingController();
+  var _enteredMessage = '';
+
+  void _sendMessage() {
+    // unfocusing the keyboard
+    FocusScope.of(context).unfocus();
+    // adding enteredMessage to the firestore
+    // adding createdAt also for ordering the message from latest to oldest
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': _enteredMessage,
+      'createdAt': Timestamp.now(),
+    });
+
+    // actually clearing the textfield after sending
+    _controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 8.0),
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              // controlling the field so it can be cleared
+              controller: _controller,
+              decoration: const InputDecoration(labelText: 'Send a message...'),
+              onChanged: (value) {
+                _enteredMessage = value;
+              },
+            ),
+          ),
+          IconButton(
+            onPressed: _enteredMessage.trim().isEmpty ? null : _sendMessage,
+            icon: Icon(
+              Icons.send,
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
