e0e192978907a9449c2b0b5ff0f84166f2fd1744
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-04-15 22:31:17 +0800

    redesign for the user

--- chat_app/lib/widgets/chat/message_bubble.dart
+++ chat_app/lib/widgets/chat/message_bubble.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
-  MessageBubble(this.message);
+  MessageBubble(
+    this.message,
+    this.isMe, {
+    required this.key,
+  });
 
   final String message;
+  final bool isMe;
+  // added key per message for better rendering
+  final Key key;
+
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
         Container(
           decoration: BoxDecoration(
-            color: Theme.of(context).accentColor,
-            borderRadius: BorderRadius.circular(12),
+            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+            borderRadius: BorderRadius.only(
+              topRight: const Radius.circular(12.0),
+              topLeft: const Radius.circular(12.0),
+              bottomLeft: !isMe
+                  ? const Radius.circular(0)
+                  : const Radius.circular(12.0),
+              bottomRight:
+                  isMe ? const Radius.circular(0) : const Radius.circular(12.0),
+            ),
           ),
           padding: const EdgeInsets.symmetric(
             vertical: 10,
@@ -25,7 +42,9 @@ class MessageBubble extends StatelessWidget {
           child: Text(
             message,
             style: TextStyle(
-              color: Theme.of(context).accentTextTheme.headline1!.color,
+              color: isMe
+                  ? Colors.black
+                  : Theme.of(context).accentTextTheme.headline1!.color,
             ),
           ),
         ),
