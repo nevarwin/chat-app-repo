f95e1b78865085c982898e8e96c068c27c290cd7
nevarwin <86557872+nevarwin@users.noreply.github.com>
2022-04-21 21:39:46 +0800

    add firestore-admin and connecting notification

--- chat_app/lib/index.js
+++ chat_app/lib/index.js
@@ -1,11 +1,20 @@
 // for firebase functions
 import { firestore } from "firebase-functions";
+import { admin } from "firebase-admin";
+
+admin.initializeApp();
 
 export const myFunction = firestore
   .document("chat/{message}")
   .onCreate((snapshot, context) => {
     console.log(snapshot.data());
-    return;
+    return admin.messaging().sendToTopic("chat", {
+      notification: {
+        title: snapshot.data().username,
+        body: snapshot.data().text,
+        clickAction: "FLUTTERNOTIFICATION_CLICK",
+      },
+    });
   });
 
 // const functions = require("firebase-functions");
